a68158f524090394b58717828ccd35ba14281f6e
mycallCampbell <mycallcampbell@gmail.com>
2023-12-04 15:38:54 +0000

    added content

--- app/page.js
+++ app/page.js
@@ -7,6 +7,7 @@ export default function Home() {
     <main>
       <h1 className={styles.titleMain}>Pat Campbell</h1>
       <Image src={'/pat_and_dad.jpg'} width={3135} height={2125} className={styles.imageHero} alt='image of Pat and Dad' quality={100} />
+      <div className={styles.date}>11th March 1947 - Forever in our Hearts</div>
       <div className={styles.tagLine}>In Loving memory of Pat Campbell</div>
       <div className={styles.iconHeartContainer}>
         <Image src={'/heart.png'} width={512} height={512} className={styles.iconHeart} alt='image of Pat and Dad' quality={100} />
